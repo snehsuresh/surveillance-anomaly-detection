fcafe9427f24b830947a479ca194093c71dd669e
snehsuresh <snehsuresh002@gmail.com>
2024-08-06 00:04:58 -0500

    proj change

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="crimedetection",
+    version="0.0.0",
+    author="Sneh",
+    author_email="snehsuresh02@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
 
 
-project_name = "signlanguage"
+project_name = "crimedetection"
 
 
 list_of_files = [
