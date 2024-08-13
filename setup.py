699c3d2869530d46565e04a1c4efc39ad77b0d5a
snehsuresh <snehsuresh002@gmail.com>
2024-08-13 18:59:19 -0500

    dir clean

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="crimedetection",
+    name="expressiondetection",
     version="0.0.0",
     author="Sneh",
     author_email="snehsuresh02@gmail.com",
